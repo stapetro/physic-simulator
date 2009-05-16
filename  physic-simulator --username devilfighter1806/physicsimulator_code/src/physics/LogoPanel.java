b8d886d70cc5f07bd351ea8a218fa95fd2c789ca
devilfighter1806 <devilfighter1806@62dbf000-2f62-11de-ba90-e1c2dc4d389a>
2009-05-16 21:37:56 +0000

    Animation panel implemented beta.

---
+++  physic-simulator --username devilfighter1806/physicsimulator_code/src/physics/LogoPanel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physics;
+
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author User
+ */
+public class LogoPanel extends JPanel {
+
+    private JLabel logoLabel;
+
+    public LogoPanel() {
+        super();
+        setSize(800, 100);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        initComponents();
+    }
+
+    private void initComponents() {
+        logoLabel = new JLabel("Here is some fucking logo!!!!");
+        add(logoLabel);
+    }
+}
---  physic-simulator --username devilfighter1806/physicsimulator_code/src/physics/MainWindow.java
+++  physic-simulator --username devilfighter1806/physicsimulator_code/src/physics/MainWindow.java
@@ -2,25 +2,44 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package physics;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import javax.swing.JFrame;
 
 /**
  * Contains all panels.
  * @author Stanislav Petrov, Krasimir Baylov
  */
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame {
 
     private AnimationPanel animationPanel;
-    private Settings settings;
+    private SettingsPanel settingsPanel;
+    private ControllerPanel controllerPanel;
+    private LogoPanel logoPanel;
     private Status status;
+    private LayoutManager layout;
 
-    public MainWindow(){
-        super();
+    public MainWindow(String name) {
+        super(name);
+        layout = new GridLayout(3, 3, 5, 5);
+        initComponents();
+    }
+
+    private void initComponents() {
+        logoPanel = new LogoPanel();
+        animationPanel = new AnimationPanel();
+        settingsPanel = new SettingsPanel(animationPanel);
+        controllerPanel = new ControllerPanel();
         setSize(800, 600);
-        setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(logoPanel, BorderLayout.NORTH);
+        add(settingsPanel, BorderLayout.WEST);
+        add(animationPanel, BorderLayout.EAST);
+        add(controllerPanel, BorderLayout.SOUTH);
+        setVisible(true);
     }
 }

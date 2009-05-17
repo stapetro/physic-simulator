d98a411db453562b65a4aad828ec3b697fe86cf1
devilfighter1806 <devilfighter1806@62dbf000-2f62-11de-ba90-e1c2dc4d389a>
2009-05-17 12:02:06 +0000

    Drawing target beta version.

---  physic-simulator --username devilfighter1806/physicsimulator_code/src/physics/Target.java
+++  physic-simulator --username devilfighter1806/physicsimulator_code/src/physics/Target.java
@@ -4,6 +4,10 @@
  */
 package physics;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
 /**
  * This class defines the basic characteristics of the target object
  * which will be drawn on the AnimationPanel.
@@ -13,6 +17,16 @@ public class Target {
     //TODO Canvas field.      -----Chech this out. I don't understand it
     //TODO setters and getters, but we will add then last.  --added by Krasi
 
+    private static final int X_LOWER_BOUND = 10;
+    private static final int X_MID_BOUND = 200;
+    private static final int X_UPPER_BOUND = 500;
+    private static final int Y_MID_UPPER_BOUND = 350;
+    private static final int Y_UPPER_BOUND = 500;
+    private static final int HEIGHT = 30;
+    /**
+     * Random generator.
+     */
+    private Random rand;
     /**
      * The x coordiante of the target
      */
@@ -21,19 +35,14 @@ public class Target {
      * The y coordinate of the target
      */
     private int y;
-    /**
-     * The width of the target
-     */
-    private int width;
-    /**
-     * The height of the target
-     */
-    private int height;
+    private Graphics targetDrawer;
 
     /**
      * Default constructor
      */
     public Target() {
+//        drawRandomCoordinates(g);
+        rand = new Random(System.currentTimeMillis());
     }
 
     /**
@@ -45,27 +54,33 @@ public class Target {
     }
 
     /**
-     * Draw the target according to the corresponding
-     * current coordinates
-     */
-    public void drawTarget() {
-        this.drawTarget(x, y);
-    }
-
-    /**
      * Draw the target on the panel of the specified coordinates
      *
      * @param xCoord    the x coordinate
      * @param yCoord    the y coordinate
      */
-    public void drawTarget(int xCoord, int yCoord) {
+    private void drawTarget(Graphics g, int xCoord, int yCoord, int height, int width) {
+        g.setColor(Color.RED);
+        g.drawOval(x, y, 30, 30);
+        g.fillOval(x, y, 30, 30);
     }
 
     /**
      * Simulates hit by making some animation
      * that attracts the attention of the user
      */
-    public void simulateHit(){
+    public void simulateHit() {
         //abe tuka neshto sharenko da stava
+    }
+
+    public void drawRandomCoordinates(Graphics g) {
+        x = X_LOWER_BOUND + rand.nextInt(X_UPPER_BOUND);
+        if (x <= X_MID_BOUND) {
+            y = rand.nextInt(Y_MID_UPPER_BOUND);
+        } else {
+            y = rand.nextInt(Y_UPPER_BOUND);
+        }
+        System.out.println("( " + x + ", " + y + " )");
+        drawTarget(g, x, y, HEIGHT, HEIGHT);
     }
 }
